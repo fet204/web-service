ef38e2b26c13018f1ceb55dfbb9594ff54ed8006
Frank Tran <306933@gmail.com>
2022-02-17 11:01:50 -0800

    initial push

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Page!</title>
+</head>
+<body>
+
+    <h1>PHP Page!</h1>
+    <p>This is HTML</p>
+    <?PHP
+        echo "<p> This is PHP</p>";
+    ?>
+
+    <p>This is HTML</p>
+    
+</body>
+</html>
